991f1968344e58d5c39b92d98c0c6f56912363fa
Joice Nicolau <joice_nicolau@hotmail.com>
2023-07-05 15:27:14 -0300

    estudando métodos

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,37 @@
+public class Metodos {
+  // Criar uma SmartTV
+
+  /*
+  1. Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+  2. Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+  3. Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+  4. Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente. 
+  */
+
+  boolean ligada = false;
+  int canal = 1;
+  int volume = 25;
+
+  void ligar() {
+    ligada = true;
+  }
+
+  void desligar() {
+    ligada = false;
+  }
+
+  public static void main(String[] args) {
+
+    Metodos tv = new Metodos();
+
+    System.out.println(tv.ligada);
+    System.out.println(tv.canal);
+    System.out.println(tv.volume);
+
+    tv.ligar();
+    System.out.println(tv.ligada);
+
+    tv.desligar();
+    System.out.println(tv.ligada);
+  }
+}
