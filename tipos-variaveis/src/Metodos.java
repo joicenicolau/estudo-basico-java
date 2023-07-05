41f3fb025aafc0027a538eb25b3f6aebc855f305
Joice Nicolau <joice_nicolau@hotmail.com>
2023-07-05 15:27:49 -0300

    metodos

--- tipos-variaveis/src/Metodos.java
+++ tipos-variaveis/src/Metodos.java
@@ -3,9 +3,7 @@ public class Metodos {
 
   /*
   1. Ela tenha as características: ligada (boolean), canal (int) e volume (int);
-  2. Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
-  3. Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
-  4. Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente. 
+  2. Nossa TV poderá ligar e desligar e assim mudar o estado ligada; 
   */
 
   boolean ligada = false;
